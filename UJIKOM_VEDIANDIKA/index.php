6559cf15d101e4c5568bb017e0a51e748d3d58bf
Vedi andika <152267276+Archiviiste@users.noreply.github.com>
2026-01-14 17:03:53 +0700

    Add files via upload

---
+++ UJIKOM_VEDIANDIKA/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        h1{
+          text-align: center;  
+        }
+
+        .button{
+            display: flex;
+            justify-content: center;
+        }
+
+        button{
+            border: none;
+            border-radius: 4px;
+            margin: 30px;
+        }
+        
+        a{
+            text-decoration: none;
+            color:inherit;
+        }
+        
+        .button1{
+            background-color: wheat;
+            width: 200px;
+            height: 40px;
+        }
+
+        .button2{
+            background-color: cyan;
+            width: 200px;
+            height: 40px;
+        }
+    </style>
+</head>
+<body>
+    <h1>SELAMAT DATANG DI</h1>
+    <h1>Website pengaduan MUTU</h1>
+
+    <div class="button">
+        <button class="button1"><a href="">Buat Pengaduan</a></button>
+        <button class="button2"><a href="">Cari Pengaduan</a></button>
+    </div>
+</body>
+</html>
