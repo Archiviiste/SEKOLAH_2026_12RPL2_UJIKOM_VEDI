d2c0909e136ffb44c132d188f31bce32019f1ab0
Vedi andika <152267276+Archiviiste@users.noreply.github.com>
2026-01-14 17:11:06 +0700

    Update halaman-pengaduan.php

--- UJIKOM_VEDIANDIKA/halaman-pengaduan.php
+++ UJIKOM_VEDIANDIKA/halaman-pengaduan.php
@@ -12,5 +12,27 @@
 </head>
 <body>
     <h1>Halaman Pengaduan</h1>
+    <form action="">
+        <div>
+            <label for="">NIS</label>
+            <br>
+            <input type="text">
+        </div>
+        <div>
+            <label for="">kategori</label>
+            <br>
+            <input type="dropdown">
+        </div>
+        <div>
+            <label for="">Lokasi</label>
+            <br>
+            <input type="text">
+        </div>
+        <div>
+            <label for="">Keterangan</label>
+            <br>
+            <textarea></textarea>
+        </div>
+    </form>
 </body>
 </html>
