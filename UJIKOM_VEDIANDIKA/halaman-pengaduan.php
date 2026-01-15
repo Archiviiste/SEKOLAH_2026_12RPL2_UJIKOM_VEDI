d1e345c072430e66057241d0f8775e931b33dc77
Vedi andika <152267276+Archiviiste@users.noreply.github.com>
2026-01-15 17:01:26 +0700

    Update halaman-pengaduan.php

--- UJIKOM_VEDIANDIKA/halaman-pengaduan.php
+++ UJIKOM_VEDIANDIKA/halaman-pengaduan.php
@@ -1,17 +1,78 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <style>
+        body{
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+        }
+
         h1{
           text-align: center;  
+        }
+
+        .box{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            border: 1px solid;
+            border-radius: 6px;
+            padding: 40px;
+        }
+
+        input{
+            width: 200px;
+        }
+
+        textarea{
+            width: 200px;
+            height: 50px;
+            resize: vertical;
+        }
+
+        .tombol{
+            display: flex;
+            justify-content: center;
+            gap: 15px;
+        }
+
+        button{
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            background-color: darkcyan;
+        }
+
+        .button1{
+            height: 30px;
+            margin-top: 10px;
+            border: none;
+            border-radius: 3px;
+            width: 95px;
+        }
+
+        .button2{
+            margin-top: 10px;
+            border: none;
+            border-radius: 3px;
+            width: 95px;
+        }
+
+
+        a{
+            text-decoration: none;
+            color:inherit;
         }
     </style>
 </head>
 <body>
     <h1>Halaman Pengaduan</h1>
+    <nav class="box">
     <form action="">
         <div>
             <label for="">NIS</label>
@@ -21,7 +82,7 @@
         <div>
             <label for="">kategori</label>
             <br>
-            <input type="dropdown">
+            <input type="text">
         </div>
         <div>
             <label for="">Lokasi</label>
@@ -33,6 +94,11 @@
             <br>
             <textarea></textarea>
         </div>
+        <div class="tombol">
+        <button class="button1"><a href="">Kirim</a></button>
+        <button class="button2"><a href="index.php">Kembali</a></button>
+        </div>
     </form>
+    </nav>
 </body>
 </html>
